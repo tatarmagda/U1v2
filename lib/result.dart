09da5230c0f9c208b70ae0792bbdf8a14de770bd
tatarmagda <tatar.magdalena@wp.pl>
2022-05-10 10:12:22 +0200

    1.2

--- lib/result.dart
+++ lib/result.dart
@@ -22,6 +22,7 @@ class Result extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
             resultPhrase,
